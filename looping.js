cc10174d8518e27d880cd978c41dfa5151c3fbf4
nithish03250 <shiliverunithishkumar775@gmail.com>
2025-01-08 13:22:11 +0530

    please help

---
+++ looping.js
@@ -0,0 +1,58 @@
+//for loop
+// for(let i=1;i<=5;i++){
+//     console.log("javascript");
+// }
+
+// for(let i=1;i<=100;i++){
+//     console.log(i);
+// }
+
+//write a program to print the 1 to 100 numbers using  loop and print the number is even or odd
+// for(let i=1;i<=100;i++){
+    // if(i%2==0){
+    //     console.log(i+" is even");
+    //     }
+    //     else{
+    //         console.log(i+" is odd");
+    //         }
+    //         }
+
+
+//write a java program to find the factorial of a number
+//5=120 ; 5*4*3*2*1
+// let num=6;
+// let factorial=1;
+// for(let i=1;i<=num;i++){
+//     factorial=factorial*i;
+//     }
+//     console.log("factorial of "+num+" is "+factorial);
+
+//write a java program to find the factors of  numbers
+//12-2,4,3,6
+// let num=12;
+// let factors=[];
+// for(let i=1;i<=num;i++){
+//     if(num%i==0){
+//         factors.push(i);
+//         }
+//         }
+//         console.log("factors of "+num+" is "+factors);
+       
+
+function addtocart(){
+
+}
+/**
+ * This function is used to add a product to the shopping cart.
+ *
+ * @param {Object} product - The product object to be added to the cart.
+ * @param {string} product.id - The unique identifier of the product.
+ * @param {string} product.name - The name of the product.
+ * @param {number} product.price - The price of the product.
+ * @param {number} product.quantity - The quantity of the product to be added to the cart.
+ *
+ * @returns {boolean} - Returns true if the product is successfully added to the cart, otherwise false.
+ */
+function addtocart(product) {
+
+}
---
+++ variable.js
@@ -0,0 +1,15 @@
+//global 
+   // console.log("start");
+   
+// var a=10;
+// console.log(a);
+
+// //local scope
+
+// var b=20;
+// console.log(b);
+
+// var c=30;
+// console.log(c);
+
+// console.log("end");
